f388c46f5209f76ac2fa9fa8f3e6fe9def8af7a2
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2025-01-14 11:41:35 +0300

    setup clerk auth with shadcn custom elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      signInUrl="/sign-in"
+      signUpUrl="/sign-up"
+    >
       <html
         lang="en"
         suppressHydrationWarning
